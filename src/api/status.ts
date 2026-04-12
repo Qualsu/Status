8e954d5ec5e47e67fb6f1fa1b7a9d4010b4c99c2
ItzKeeni <maratkanovartem89@mail.ru>
2026-04-12 21:36:23 +0300

    move fetch to api file

--- src/api/status.ts
+++ src/api/status.ts
@@ -1,6 +1,28 @@
 import { api } from '../config/const/api.const';
 import { API } from '../config/routing/api.route';
 import type { PingResponse, PingStatusResult } from '../config/types/api.types';
+import type { ServiceConfig, ServiceEndpointStatus, ServiceStatus, Statuses } from '../config/types/components.types';
+
+export async function fetchServiceStatus(service: ServiceConfig): Promise<ServiceStatus> {
+  const getOverallStatus = (statuses: Statuses[]): Statuses =>
+	statuses.includes('error') ? 'error' : 
+	statuses.includes('offline') ? 'offline' : 
+	'online';
+
+  const endpointStatuses: ServiceEndpointStatus[] = await Promise.all(
+	service.endpoints.map(async (endpoint) => {
+	  const result = await pingWebsite(endpoint.url);
+	  return { ...endpoint, ...result };
+	})
+  );
+
+  return {
+	key: service.key,
+	label: service.label,
+	status: getOverallStatus(endpointStatuses.map((endpoint) => endpoint.status)),
+	endpoints: endpointStatuses,
+  };
+}
 
 export async function pingWebsite(url: string): Promise<PingStatusResult> {
 	try {
